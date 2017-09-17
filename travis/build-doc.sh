1dac19c1a5b0dfabd1d0a9066251ff6178aa9619
Christophe Ecabert <christophe.ecabert@gmail.com>
2017-09-17 21:03:52 +0200

    CHG: .

--- travis/build-doc.sh
+++ travis/build-doc.sh
@@ -4,4 +4,5 @@ echo "Build Documentation"
 cd $TRAVIS_BUILD_DIR
 if [[ "$DOCUMENTATION" == "TRUE" ]]; then
   cd build && make doc
+  ls modules/doc/html
 fi
--- travis/deploy-doc.sh
+++ travis/deploy-doc.sh
@@ -14,16 +14,13 @@ if [ -d "$TRAVIS_BUILD_DIR/build/modules/doc/html" ]; then
     git clone $REPO --quiet --depth 1 --branch=gh-pages doc/html &>/dev/null
     
     # Copy new doc
-    rm -rf doc/html/{search,*.png,*.css,*.js,*.html}
+    #rm -rf doc/html/{search,*.png,*.css,*.js,*.html}
     cp -R modules/doc/html/* doc/html/
-    echo "Already in"
-    ls doc/html
-    echo "Newly generated"
     ls modules/doc/html
     
     # Commit
     pushd doc/html
-    git add --force .
+    git add --all .
     git commit --allow-empty -m "Update documentation to ${TRAVIS_COMMIT:0:7}"
     git push ${SSH_REPO} gh-pages
     popd
