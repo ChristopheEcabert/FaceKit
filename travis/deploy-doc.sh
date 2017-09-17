e8bdcca1c2d5c8213b73b165089f9d77132e2cac
Christophe Ecabert <christophe.ecabert@gmail.com>
2017-09-17 20:38:09 +0200

    CHG: .

--- travis/deploy-doc.sh
+++ travis/deploy-doc.sh
@@ -3,12 +3,12 @@ set -e
 echo "Deploy - Documentation"
 if [ -d "$TRAVIS_BUILD_DIR/build/modules/doc/html" ]; then
   cd $TRAVIS_BUILD_DIR/build
-  echo "Check if documentation can be updated"
   if [[ "$TRAVIS_PULL_REQUEST" == "false" && "$TRAVIS_BRANCH" == "develop" && "$TRAVIS_REPO_SLUG" == "ChristopheEcabert/FaceKit" ]]; then
     echo "Get exisitin documentation"
     # Suppress output to avoid leaking the token when the command fails
     REPO=`git config remote.origin.url`
-    #SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
+    SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
+    SHA=`git rev-parse --verify HEAD`
     
     # Clone existing gh-pages
     git clone $REPO --quiet --depth 1 --branch=gh-pages doc/html &>/dev/null
@@ -21,7 +21,7 @@ if [ -d "$TRAVIS_BUILD_DIR/build/modules/doc/html" ]; then
     pushd doc/html
     git add .
     git commit --allow-empty -m "Update documentation to ${TRAVIS_COMMIT:0:7}"
-    git push origin gh-pages
+    git push ${SSH_REPO} gh-pages
     popd
   fi
 fi
