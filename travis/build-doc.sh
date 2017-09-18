5aa4201f70c66db25bf2c1d462feeac55c1ce1ad
Christophe Ecabert <christophe.ecabert@gmail.com>
2017-09-18 10:39:47 +0200

    CHG: .

--- travis/build-doc.sh
+++ travis/build-doc.sh
@@ -3,8 +3,8 @@ set -e
 echo "Build Documentation"
 cd $TRAVIS_BUILD_DIR
 if [[ "$DOCUMENTATION" == "TRUE" ]]; then
-  echo "Already some doc ?"
-  cd build && ls modules/doc
+  #echo "Already some doc ?"
+  #cd build && ls modules/doc
   echo "Generate"
   cd build && make doc
   echo "Check"
