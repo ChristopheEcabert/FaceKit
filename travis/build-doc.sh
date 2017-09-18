d17b5223e52e9dc8d9265b76e8b972ca761789f1
Christophe Ecabert <christophe.ecabert@gmail.com>
2017-09-18 10:08:48 +0200

    CHG: .

--- travis/build-doc.sh
+++ travis/build-doc.sh
@@ -3,7 +3,12 @@ set -e
 echo "Build Documentation"
 cd $TRAVIS_BUILD_DIR
 if [[ "$DOCUMENTATION" == "TRUE" ]]; then
+  echo "Already some doc ?"
+  ls modules/doc/html
+  echo "Generate"
   cd build && make doc
+  echo "Check"
+  pwd
   cat modules/doc/Doxyfile
   ls modules/doc/html
 fi
