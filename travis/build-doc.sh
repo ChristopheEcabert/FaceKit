ede20aa772804f701f3115c0fca7e200db7f88fd
Christophe Ecabert <christophe.ecabert@gmail.com>
2017-09-26 09:45:31 +0200

    CHG: Refactor documentation gen

--- travis/build-doc.sh
+++ travis/build-doc.sh
@@ -3,12 +3,6 @@ set -e
 echo "Build Documentation"
 cd $TRAVIS_BUILD_DIR
 if [[ "$DOCUMENTATION" == "TRUE" ]]; then
-  #echo "Already some doc ?"
-  #cd build && ls modules/doc
   echo "Generate"
   cd build && make doc
-  echo "Check"
-  pwd
-  ls ../modules
-  ls ../modules/core/include/facekit/core
 fi
