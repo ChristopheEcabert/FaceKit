aa60f7d5311a9a9820e6e87f48f7e677d53f731b
Christophe Ecabert <christophe.ecabert@gmail.com>
2018-02-16 09:59:44 +0100

    .

--- travis/build-library.sh
+++ travis/build-library.sh
@@ -5,4 +5,5 @@ echo "Build Library"
 export PATH=${OPENBLAS_ROOT}:${OPENCV_ROOT}:${DOXYGEN_ROOT}/bin:${PROTOBUF_ROOT}/bin:${PROTOBUF_ROOT}:${PATH}
 cd $TRAVIS_BUILD_DIR
 echo "Current path: ${PATH}"
-mkdir -p build && cd build && cmake ${CMAKE_OPTIONS} -DProtobuf_DEBUG=ON .. && make -j8 
+protoc --version
+mkdir -p build && cd build && cmake ${CMAKE_OPTIONS} -DProtobuf_DEBUG=ON -DProtobuf_INCLUDE_DIR=${PROTOBUF_ROOT}/include .. && make -j8 
