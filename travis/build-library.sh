42c54bff276a5af5d719f9c7a970c6df96ae488e
Christophe Ecabert <christophe.ecabert@gmail.com>
2018-02-16 09:54:35 +0100

    .

--- travis/build-library.sh
+++ travis/build-library.sh
@@ -2,8 +2,7 @@
 set -e
 echo "Build Library"
 # Update path for finding deps
-export PATH=${OPENBLAS_ROOT}:${OPENCV_ROOT}:${DOXYGEN_ROOT}/bin:${PROTOBUF_ROOT}:${PATH}
+export PATH=${OPENBLAS_ROOT}:${OPENCV_ROOT}:${DOXYGEN_ROOT}/bin:${PROTOBUF_ROOT}/bin:${PROTOBUF_ROOT}:${PATH}
 cd $TRAVIS_BUILD_DIR
 echo "Current path: ${PATH}"
-ls ${OPENBLAS_ROOT}/lib
 mkdir -p build && cd build && cmake ${CMAKE_OPTIONS} -DProtobuf_DEBUG=ON .. && make -j8 
