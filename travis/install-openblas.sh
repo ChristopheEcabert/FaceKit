827438b713e89363f39030111a68bddee6db9b32
Christophe Ecabert <christophe.ecabert@gmail.com>
2018-03-08 10:34:44 +0100

    CHG: Add matching gfortran version

--- travis/install-openblas.sh
+++ travis/install-openblas.sh
@@ -7,23 +7,25 @@ LOCAL_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 if [ "$#" -ne 1 ];
 then
-    echo "Usage: $0 INSTALL_DIR"
-    exit 1
+  echo "Usage: $0 INSTALL_DIR"
+  exit 1
 fi
 
 INSTALL_DIR=`readlink -f $1`
 if [ -d "$INSTALL_DIR" ] && ls $INSTALL_DIR/lib/*.so >/dev/null 2>&1; then
-    echo "Using cached build at $INSTALL_DIR ..."
+  echo "Using cached build at $INSTALL_DIR ..."
 else
-    rm -rf $INSTALL_DIR
-    git clone --depth 1 https://github.com/xianyi/OpenBLAS.git ${INSTALL_DIR}/source 
-    cd $INSTALL_DIR/source
-    # Redirect build output to a log and only show it if an error occurs
-    # Otherwise there is too much output for TravisCI to display properly
-    make NO_AFFINITY=1 USE_THREAD=0 -j8
-    make install PREFIX=$INSTALL_DIR
-    # Remove source
-    cd ${INSTALL_DIR} && rm -rf ${INSTALL_DIR}/source
+  $CC -v
+  $FC -v
+  rm -rf $INSTALL_DIR
+  git clone --depth 1 https://github.com/xianyi/OpenBLAS.git ${INSTALL_DIR}/source 
+  cd $INSTALL_DIR/source
+  # Redirect build output to a log and only show it if an error occurs
+  # Otherwise there is too much output for TravisCI to display properly
+  make FC=$FC NO_AFFINITY=1 USE_THREAD=0 -j8
+  make install PREFIX=$INSTALL_DIR
+  # Remove source
+  cd ${INSTALL_DIR} && rm -rf ${INSTALL_DIR}/source
 fi
 # Update path file
 export PATH=$INSTALL_DIR:${PATH}
