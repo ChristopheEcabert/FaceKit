c09e38a3d3f60a646c3500ba147d94e41ea81340
Christophe Ecabert <christophe.ecabert@gmail.com>
2018-03-08 11:14:01 +0100

    .

--- travis/install-openblas.sh
+++ travis/install-openblas.sh
@@ -16,13 +16,13 @@ if [ -d "$INSTALL_DIR" ] && ls $INSTALL_DIR/lib/*.so >/dev/null 2>&1; then
   echo "Using cached build at $INSTALL_DIR ..."
 else
   echo `$CC -v`
-  echo `$FC --version`
+  echo "Fotran compiler: $FC"
   rm -rf $INSTALL_DIR
   git clone --depth 1 https://github.com/xianyi/OpenBLAS.git ${INSTALL_DIR}/source 
   cd $INSTALL_DIR/source
   # Redirect build output to a log and only show it if an error occurs
   # Otherwise there is too much output for TravisCI to display properly
-  make FC=$FC NO_AFFINITY=1 USE_THREAD=0 -j8
+  make FC="$FC" NO_AFFINITY=1 USE_THREAD=0 -j8
   make install PREFIX=$INSTALL_DIR
   # Remove source
   cd ${INSTALL_DIR} && rm -rf ${INSTALL_DIR}/source
