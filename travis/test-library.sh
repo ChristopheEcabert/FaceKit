c1a2c15b58ba12106b59acb23db33130d8b6135d
Christophe Ecabert <christophe.ecabert@gmail.com>
2018-05-30 12:33:49 +0200

    CHG: Update testing script to ensure proper return code

--- travis/test-library.sh
+++ travis/test-library.sh
@@ -1,6 +1,9 @@
 #! /bin/bash
-set -e
 echo "Call Unit Test"
 cd $TRAVIS_BUILD_DIR
-cd build && make test
+cd build
+if make test; then
+  exit 0
+fi
+exit -1
 # Test output are only shown if failure happened
