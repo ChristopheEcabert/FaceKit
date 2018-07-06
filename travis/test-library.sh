ebc31a462f244bb6363f2f1b05dca6c4d040edba
Christophe Ecabert <christophe.ecabert@gmail.com>
2018-07-06 16:53:39 +0200

    CHG: Update testing script to show unit-test output

--- travis/test-library.sh
+++ travis/test-library.sh
@@ -1,9 +1,4 @@
 #! /bin/bash
 echo "Call Unit Test"
-cd $TRAVIS_BUILD_DIR
-cd build
-if make test > /dev/null; then
-  exit 0
-fi
-exit -1
-# Test output are only shown if failure happened
+cd $TRAVIS_BUILD_DIR/build
+make test
